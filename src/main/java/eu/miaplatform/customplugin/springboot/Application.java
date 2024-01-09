e46b6b650f28220f5de81c75bb5b52449ded18e9
Davide Calabrò <davide.calabro@mia-platform.eu>
2024-01-09 17:52:32 +0100

    Add springboot-starter-web

--- src/main/java/eu/miaplatform/customplugin/springboot/Application.java
+++ src/main/java/eu/miaplatform/customplugin/springboot/Application.java
@@ -1,10 +1,18 @@
-package eu.miaplatform.customplugin.springboot.springboottemplate;
+package eu.miaplatform.customplugin.springboot;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 
+@RestController
 @SpringBootApplication
 public class Application {
+
+	@RequestMapping("/")
+	String home() {
+		return "Hello World!";
+	}
 
 	public static void main(String[] args) {
 		SpringApplication.run(Application.class, args);
--- src/test/java/eu/miaplatform/customplugin/springboot/SpringbootTemplateApplicationTests.java
+++ src/test/java/eu/miaplatform/customplugin/springboot/SpringbootTemplateApplicationTests.java
@@ -1,4 +1,4 @@
-package eu.miaplatform.customplugin.springboot.springboottemplate;
+package eu.miaplatform.customplugin.springboot;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
